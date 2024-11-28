df5d8be2cc7ae1a4950a915e1a8a6d777a4d424d
JosiasHenrique <joystickhenrii@gmail.com>
2024-11-28 20:19:28 -0300

    Header e About

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import About from "./components/about";
 import Header from "./components/header";
+import Projects from "./components/projects";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <About />
+      <Projects />
     </main>
   );
 }
---
+++ src/components/about.jsx
@@ -0,0 +1,27 @@
+
+export default function About() {
+    return (
+        <section className="section-about">
+            <div className="container mx-auto py-16 px-10">
+            <div className="divider">
+                <span>Sobre mim</span>
+            </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                    <div>
+                        <p className="text-lg mb-6">
+                            Olá, sou <strong>Josias</strong>, um estudante de Engenharia de Software fascinado por tecnologia e desenvolvimento.
+                            Atualmente estou no 5º semestre da faculdade e foco meu aprendizado principalmente no desenvolvimento Frontend, com ênfase em <strong>React.js</strong> e <strong>Next.js</strong>.
+                            Além disso, estou me aprofundando em <strong>TypeScript</strong> e tenho interesse em back-end com <strong>Node.js</strong>.
+                        </p>
+                        <p className="text-lg mb-6">
+                            Tenho experiência como <strong>Técnico em Informática</strong>, onde aprendi a trabalhar com hardware e solucionar problemas de software. Embora ainda esteja no início da minha carreira como desenvolvedor, estou buscando uma oportunidade de estágio para colocar em prática o que aprendi e continuar evoluindo.
+                        </p>
+                    </div>
+                    <div className="flex justify-center items-center">
+                        <img src="/img/arte.svg" alt="Foto de Onyedika" className="rounded-lg shadow-lg w-full max-w-md" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
