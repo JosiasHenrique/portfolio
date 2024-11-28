f0189fc2a65232b48d3c8a5547ce4f60eae6c3d8
JosiasHenrique <joystickhenrii@gmail.com>
2024-11-28 13:56:17 -0300

    Iniciando portfolio

---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="logo">
+                <img src="" alt="logo" />
+            </div>
+        </nav>
+    )
+}
